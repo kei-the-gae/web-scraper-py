9c2cecd92e393c5e9fc498550e82db9642d27dc2
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-08-13 13:10:42 -0700

    add cli parsing

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import sys
+
+
 def main():
-    print("Hello from web-scraper!")
+    args = sys.argv
+    if len(args) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(args) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    base_url = args[1]
+
+    print(f"starting crawl of: {base_url}...")
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
