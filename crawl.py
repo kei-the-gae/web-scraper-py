91aa0141eef774b334788fffcbd02784b2d503b6
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-08-13 14:25:40 -0700

    add recursion to site scraping

--- crawl.py
+++ crawl.py
@@ -20,17 +20,48 @@ def get_urls_from_html(html, base_url):
             try:
                 absolute_url = urljoin(base_url, href)
                 urls.append(absolute_url)
-            except Exception as e:
-                print(f"{str(e)}: {href}")
+            except Exception as err:
+                print(f"{str(err)}: {href}")
 
     return urls
+
+
+def crawl_page(base_url, current_url=None, pages=None):
+    if current_url is None:
+        current_url = base_url
+    if pages is None:
+        pages = {}
+
+    base_url_obj = urlparse(base_url)
+    current_url_obj = urlparse(current_url)
+    if current_url_obj.netloc != base_url_obj.netloc:
+        return pages
+
+    normalized_url = normalize_url(current_url)
+
+    if normalized_url in pages:
+        pages[normalized_url] += 1
+        return pages
+
+    pages[normalized_url] = 1
+
+    print(f"crawling {current_url}")
+    html = safe_get_html(current_url)
+    if html is None:
+        return pages
+
+    next_urls = get_urls_from_html(html, base_url)
+    for next_url in next_urls:
+        pages = crawl_page(base_url, next_url, pages)
+
+    return pages
 
 
 def get_html(url):
     try:
         res = requests.get(url)
-    except Exception as e:
-        raise Exception(f"network error while fetching {url}: {e}")
+    except Exception as err:
+        raise Exception(f"network error while fetching {url}: {err}")
 
     if res.status_code > 399:
         raise Exception(f"got HTTP error: {res.status_code} {res.reason}")
@@ -40,3 +71,11 @@ def get_html(url):
         raise Exception(f"got non-HTML response: {content_type}")
 
     return res.text
+
+
+def safe_get_html(url):
+    try:
+        return get_html(url)
+    except Exception as err:
+        print(f"{err}")
+        return None
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from crawl import get_html
+from crawl import crawl_page
 
 
 def main():
@@ -16,12 +16,12 @@ def main():
     print(f"starting crawl of: {base_url}...")
 
     try:
-        html = get_html(base_url)
-    except Exception as e:
-        print(f"Error fetching HTML from {base_url}: {str(e)}")
+        pages = crawl_page(base_url)
+    except Exception as err:
+        print(f"Error fetching HTML from {base_url}: {str(err)}")
         sys.exit(1)
 
-    print(html)
+    print(pages)
 
     sys.exit(0)
 
